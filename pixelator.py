d67a9ce1c44e2a435977e1bd094bd26e7f6ffaa1
simodecl <decloedt.simon@gmail.com>
2018-09-24 14:07:30 +0200

    pixelator.py created

--- pixelator.py
+++ pixelator.py
@@ -4,6 +4,7 @@ from random import randint
 
 sense = SenseHat()
 
+O = [0, 0, 0]  # Black
 pixels = [
 O, O, O, O, O, O, O, O,
 O, O, O, O, O, O, O, O,
@@ -14,3 +15,17 @@ O, O, O, O, O, O, O, O,
 O, O, O, O, O, O, O, O,
 O, O, O, O, O, O, O, O,
 ]
+sense.set_pixels(pixels)
+
+i = 0
+while True:
+    #i staat voor index, p staat voor de pixel value (de value moet niet opgeroepen worden)
+    for i, p in enumerate(pixels):
+        pixels[i-1] = O
+        pixels[i] = [randint(0,255), randint(0,255), randint(0,255)]
+        sense.set_pixels(pixels)
+        time.sleep(0.2)
+        i + 1
+        if i > 63:
+            i = 0
+    
