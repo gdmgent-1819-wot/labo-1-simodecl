75edf4d6f14a39d873db072faf107551a3803e71
simodecl <decloedt.simon@gmail.com>
2018-09-24 13:17:57 +0200

    letters.py updated

--- letters.py
+++ letters.py
@@ -4,16 +4,12 @@ from random import randint
 
 sense = SenseHat()
 
-#Letters in array zetten om te kunnen gebruiken in de loop
-letters = ["N","M","D"]
+letters = "NMD"
 
-i = 0
-while i < len(letters):
-    #Array doorlopen en willekeurige kleur meegeven
-    sense.show_letter(letters[i], text_colour=[randint(0,256),randint(0,256),randint(0,256)])
-    print(i)
-    i = i + 1
-    #Na iedere loop een seconde pauzeren
-    time.sleep(1)
-    if i > 2:
-        i = 0
+#While loop altijd laten doorlopen
+while True:
+    for letter in letters:
+        #Array doorlopen en willekeurige kleur meegeven
+        sense.show_letter(letter, text_colour=[randint(0,255),randint(0,255),randint(0,255)])
+        #Na iedere loop een seconde pauzeren
+        time.sleep(1)
---
+++ pixelator.py
@@ -0,0 +1,16 @@
+from sense_hat import SenseHat
+import time
+from random import randint
+
+sense = SenseHat()
+
+pixels = [
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+]
